2d8cea8f8bcda181fb4348009cbe4f3b8f593e59
bozhiyeh <ddd22251444@gmail.com>
2020-05-03 17:51:19 +0800

    revert test

---
+++ src/test/java/test/java/domain/LabInventory/LabInventoryTest.java
@@ -0,0 +1,40 @@
+package test.java.domain.LabInventory;
+
+import domain.inventory.LabInventory;
+import domain.labMember.LabMember;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class LabInventoryTest {
+
+    private LabInventory labInventory;
+
+    @Before
+    public void setup(){
+        LabMember labMember = new LabMember("Bill", "xxx@gmail.com", "123");
+        labInventory = new LabInventory("groceries", "tissue", 1, labMember);
+    }
+
+    @Test
+    public void inventoryAmountTest(){
+        assertEquals(1, labInventory.getAmount());
+        labInventory.add(1);
+        assertEquals(2, labInventory.getAmount());
+        labInventory.minus(1);
+        assertEquals(1, labInventory.getAmount());
+    }
+
+    @Test
+    public void inventoryLimitTest(){
+        labInventory.setLimitNum(3);
+        labInventory.setAmount(2);
+        assertTrue(labInventory.isUnderLimit());
+        labInventory.add(1);
+        assertTrue(!labInventory.isUnderLimit());
+    }
+}
---
+++ src/test/java/test/java/domain/controller/ControllerTest.java
@@ -0,0 +1,106 @@
+package test.java.domain.controller;
+
+import domain.controller.Controller;
+import domain.inventory.LabInventory;
+import domain.labMember.Admin;
+import domain.labMember.LabMember;
+import domain.labMember.Manager;
+import domain.labMember.Viewer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+    private LabMember labMember;
+    private Controller controller;
+
+    @Before
+    public void setup(){
+        labMember = new Manager("Bill", "xxx@gmail.com", "123");
+        controller = new Controller();
+    }
+
+    @Test
+    public void createInventoryTest(){
+        controller.createInventory("groceries", "tissue", 1, labMember);
+        assertEquals("tissue", controller.getLabInventoryList().selectCategory("groceries").get(0).getName());
+    }
+
+    @Test
+    public void readInventoryTest(){
+        controller.createInventory("groceries", "tissue", 1, labMember);
+        LabInventory reader = controller.readInventory("groceries", "tissue");
+        assertEquals("tissue", reader.getName());
+    }
+
+    @Test
+    public void updateInventoryAmountTest(){
+        controller.createInventory("groceries", "tissue", 1, labMember);
+        controller.updateInventoryAmount("groceries", "tissue", 5, labMember);
+        assertEquals(5, controller.readInventory("groceries", "tissue").getAmount());
+    }
+
+    @Test
+    public void updateInventoryNameTest(){
+        controller.createInventory("groceries", "tissue", 1, labMember);
+        controller.updateInventoryName("groceries", "tissue", "new tissue", labMember);
+        assertEquals("new tissue", controller.readInventory("groceries", "new tissue").getName());
+    }
+
+    @Test
+    public void deleteInventoryNameTest(){
+        controller.createInventory("groceries", "tissue", 1, labMember);
+        assertNotNull(controller.readInventory("groceries", "tissue"));
+        controller.deleteInventory("groceries", "tissue", labMember);
+        assertNull(controller.readInventory("groceries", "tissue"));
+    }
+
+    @Test
+    public void isInventoryUnderLimitTest(){
+        controller.createInventory("groceries", "tissue", 1, labMember);
+        controller.readInventory("groceries", "tissue").setLimitNum(3);
+        assertTrue(controller.IsInventoryUnderLimit("groceries", "tissue"));
+        controller.updateInventoryAmount("groceries", "tissue", 5, labMember);
+        assertTrue(!controller.IsInventoryUnderLimit("groceries", "tissue"));
+    }
+
+    @Test
+    public void createAndReadCategoryTest(){
+        controller.createCategory("groceries", labMember);
+        assertNotNull(controller.readCategory("groceries"));
+        assertEquals(0, controller.readCategory("groceries").size());
+    }
+
+    @Test
+    public void updateCategoryNameTest(){
+        controller.createCategory("groceries", labMember);
+        controller.createInventory("groceries", "tissue", 2, labMember);
+        controller.updateCategoryName("groceries", "new groceries", labMember);
+        assertNull(controller.readCategory("groceries"));
+        assertNotNull(controller.readCategory("new groceries"));
+        assertEquals("tissue", controller.readCategory("new groceries").get(0).getName());
+    }
+
+    @Test
+    public void deleteCategoryTest(){
+        controller.createCategory("groceries", labMember);
+        assertNotNull(controller.readCategory("groceries"));
+        controller.deleteCategory("groceries", labMember);
+        assertNull(controller.readCategory("groceries"));
+    }
+
+    @Test
+    public void setUserPermissionTest() {
+        LabMember labMember = new Admin("Jeff", "jeff@gmail.com", "123");
+        LabMember newLabMember = new Viewer("newJeff", "newJeff@gmail.com", "456");
+        assertEquals("Viewer", newLabMember.getPermission());
+        controller.setUserPermission(labMember, "Manager", newLabMember);
+        assertEquals("Manager", newLabMember.getPermission());
+
+        assertEquals("Admin", labMember.getPermission());
+        controller.setUserPermission(newLabMember, "Manager", labMember);
+        assertEquals("Admin", labMember.getPermission());
+    }
+
+}
