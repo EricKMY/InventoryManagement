ff1b856341544ee470bd342b6ce43c5af3c2c0cc
tzutseyang <tzutse.yang@gmail.com>
2020-06-22 19:51:59 +0800

    modify gmail

--- src/main/java/domain/notificationInfo/NotificationInfo.java
+++ src/main/java/domain/notificationInfo/NotificationInfo.java
@@ -81,9 +81,9 @@ public class NotificationInfo implements INotificationInfo {
 
     public void sendAlert() {
 
-        final String from = "erickuan970810edu@gmail.com";
-        final String password = "Js95816973edu";
-        final String to = "ntutlab1321mmc@gmail.com";
+        final String from = "ntutlab1321mmc@gmail.com";
+        final String password = "lab1321*";
+        final String to = "erickuan970810edu@gmail.com";
         String sub;
         String msg;
 
@@ -121,34 +121,5 @@ public class NotificationInfo implements INotificationInfo {
         } catch (MessagingException e) {throw new RuntimeException(e);}
 
     }
-
-//    public void sendAlert(final String from, final String password, String to, String sub, String msg) {
-//        //Get properties object
-//        Properties props = new Properties();
-//        props.put("mail.smtp.host", "smtp.gmail.com");
-//        props.put("mail.smtp.socketFactory.port", "465");
-//        props.put("mail.smtp.socketFactory.class",
-//                "javax.net.ssl.SSLSocketFactory");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.port", "465");
-//        //get Session
-//        Session session = Session.getDefaultInstance(props,
-//                new javax.mail.Authenticator() {
-//                    protected PasswordAuthentication getPasswordAuthentication() {
-//                        return new PasswordAuthentication(from,password);
-//                    }
-//                });
-//        //compose message
-//        try {
-//            MimeMessage message = new MimeMessage(session);
-//            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
-//            message.setSubject(sub);
-//            message.setText(msg);
-//            //send message
-//            Transport.send(message);
-//            System.out.println("message sent successfully");
-//        } catch (MessagingException e) {throw new RuntimeException(e);}
-//
-//    }
 
 }
